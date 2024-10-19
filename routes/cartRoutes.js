f3fe5849b7f74060215761b388394e45de701e66
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-19 21:50:54 +0100

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,27 +1,17 @@
 const express = require('express');
-const { addToCart, getCart, removeFromCart, clearCart } = require('../controllers/cartController');
-const { protect } = require('../middleware/authMiddleware'); // Assuming you have a middleware for authentication
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const {
+    addToCart,
+    getCart,
+    removeFromCart,
+    clearCart
+} = require('../controllers/cartController');
 
-const CartRoutes = express.Router();
+// Add userId as a URL parameter
+router.post('/:userId', addToCart);
+router.get('/:userId', getCart); // Adjusted to get cart for a specific user
+router.delete('/:userId/:id', removeFromCart); // Adjusted to remove from cart for a specific user
+router.delete('/:userId', clearCart); // Adjusted to clear cart for a specific user
 
-// @route POST /api/cart
-// @desc Add product to cart
-// @access Private
-router.post('/',  addToCart);
-
-// @route GET /api/cart
-// @desc Get user's cart
-// @access Private
-router.get('/',  getCart);
-
-// @route DELETE /api/cart/:id
-// @desc Remove product from cart
-// @access Private
-router.delete('/:id', removeFromCart);
-
-// @route DELETE /api/cart
-// @desc Clear cart after order
-// @access Private
-router.delete('/', protect, clearCart);
-
-module.exports = CartRoutes;
+module.exports = router;
