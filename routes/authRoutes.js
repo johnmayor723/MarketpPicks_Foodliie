091b3a347c47bc5da77b35dd5501517ee5778fdc
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-01-23 09:30:42 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { register, login, validateCoupon, updateCouponValue } = require('../controllers/authController');
+const { register, login, activateCoupon, validateCoupon, updateCouponValue } = require('../controllers/authController');
 const router = express.Router();
+
+// user's authentication routes
 
 router.post('/register', register);
 router.post('/login', login);
+
+//user's coupon routes
+
+// Validate Active Coupon
+router.post('/activate-coupon', activateCoupon);
+
 // Validate Active Coupon
 router.post('/validate-coupon', validateCoupon);
 
