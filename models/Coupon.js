98dc9b391966a3c6d9dd24a77e63a1415bef7162
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-01-24 17:26:30 +0100

    Update Coupon.js

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const CouponSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  couponCode: { type: String, required: true, unique: true }, // Unique identifier
-  agentIdentifier: { type: String, required: true },
+  couponId: { type: String, required: true, unique: true },
+  couponCode: { type: String, required: true }, // Unique identifier
   value: { type: Number, default: 50000,  required: true }, // Remaining coupon value
   isValid: { type: Boolean, default: true },
   expiryDate: { type: Date, required: true }, // Last day of the month
