e16c95fce38e5b879b6ee1516678c4c8f7a09677
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2025-01-24 17:22:32 +0100

    Update Agent.js

--- models/Agent.js
+++ models/Agent.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const AgentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  couponIdentifier: { type: String, required: true },
+  couponCode: { type: String, required: true },
   totalSales: { type: Number, default: 0 },
 });
 
