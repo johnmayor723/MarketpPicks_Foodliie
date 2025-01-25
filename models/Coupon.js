6d2fe9532ce6a4113ec85ed66e329b99ee3288e5
may <may@xymail.com>
2025-01-25 11:30:26 +0100

    mm

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose")
+
 const couponSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   couponId: { type: String, required: true, unique: true },
